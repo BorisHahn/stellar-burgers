e6e05694cb8f2af0a7f3682c031dcb316e169702
Boris Gaidukevich <Borishahn@yandex.ru>
2023-02-02 01:49:27 +0300

    AppHeader fix

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -10,28 +10,30 @@ const classNames = require('classnames');
 const AppHeader = () => {
   return (
     <header className='header'>
-      <ul className='header__nav-list'>
-        <li className='header__nav-item'>
-          <BurgerIcon type='primary' />
-          <p className='text text_type_main-default'>Конструктор</p>
-        </li>
-        <li className='header__nav-item'>
-          <ListIcon type='secondary' />
+      <div className='header__nav'>
+        <ul className='header__nav-list'>
+          <li className='header__nav-item'>
+            <BurgerIcon type='primary' />
+            <p className='text text_type_main-default'>Конструктор</p>
+          </li>
+          <li className='header__nav-item'>
+            <ListIcon type='secondary' />
+            <p className='text text_type_main-default text_color_inactive'>
+              Лента заказов
+            </p>
+          </li>
+        </ul>
+        <span className='header__logo'>
+          <Logo />
+        </span>
+        <div
+          className={classNames('pl-5 pr-5 pb-4 pt-4', 'header__personal-area')}
+        >
+          <ProfileIcon type='secondary' />
           <p className='text text_type_main-default text_color_inactive'>
-            Лента заказов
+            Личный кабинет
           </p>
-        </li>
-      </ul>
-      <span className='header__logo'>
-        <Logo />
-      </span>
-      <div
-        className={classNames('pl-5 pr-5 pb-4 pt-4', 'header__personal-area')}
-      >
-        <ProfileIcon type='secondary' />
-        <p className='text text_type_main-default text_color_inactive'>
-          Личный кабинет
-        </p>
+        </div>
       </div>
     </header>
   );
