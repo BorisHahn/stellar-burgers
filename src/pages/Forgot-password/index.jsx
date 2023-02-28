dd0432ab88dba3dae2361a914390b35cdd347558
Boris Gaidukevich <Borishahn@yandex.ru>
2023-03-01 01:18:53 +0300

    добавлена функциональность сброса пароля, компонент профиля

--- src/pages/Forgot-password/index.jsx
+++ src/pages/Forgot-password/index.jsx
@@ -1,15 +1,44 @@
 import styles from './Forgot-password.module.css';
 import classNames from 'classnames';
+import Spinner from 'react-bootstrap/Spinner';
 import { emailRegExp } from '../../utils/const';
 import { useState, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import {
   Input,
   Button,
 } from '@ya.praktikum/react-developer-burger-ui-components';
 const ForgotPassword = () => {
+  const navigate = useNavigate();
   const [emailValue, setEmailValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef(null);
+
+  async function resetPassword(data) {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        'https://norma.nomoreparties.space/api/password-reset',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email: `${data}` }),
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        navigate('/reset-password');
+        setEmailValue('');
+        setIsLoading(false);
+        return data;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <section className={styles.forgotPassword}>
       <h2 className={classNames(styles.title, 'text text_type_main-medium')}>
@@ -29,8 +58,28 @@ const ForgotPassword = () => {
         extraClass='mb-6'
         required
       />
-      <Button htmlType='button' type='primary' size='large' extraClass='mb-20'>
-        Восстановить
+      <Button
+        htmlType='button'
+        type='primary'
+        size='large'
+        extraClass='mb-20'
+        onClick={() => resetPassword(emailValue)}
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <>
+            <Spinner
+              as='span'
+              animation='border'
+              size='sm'
+              role='status'
+              aria-hidden='true'
+            />{' '}
+            Ожидание...
+          </>
+        ) : (
+          'Восстановить'
+        )}
       </Button>
       <p
         className={classNames(
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,102 @@
 import styles from './Profile.module.css';
+import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import classNames from 'classnames';
+import { emailRegExp } from '../../utils/const';
+import { useState, useRef } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 const Profile = () => {
+  const [nameValue, setNameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const inputRef = useRef(null);
+  const location = useLocation();
+
   return (
     <section className={styles.profile}>
-      <p>Страница профиля</p>
+      <nav className={styles.navigation}>
+        <NavLink
+          to={'/profile'}
+          className={classNames(
+            styles.link,
+            location.pathname === '/profile'
+              ? ['text text_type_main-medium', styles.linkActive]
+              : 'text text_type_main-medium text_color_inactive',
+          )}
+        >
+          Профиль
+        </NavLink>
+        <NavLink
+          to={'/profile/orders'}
+          className={classNames(
+            styles.link,
+            location.pathname === '/profile/order'
+              ? ['text text_type_main-medium', styles.linkActive]
+              : 'text text_type_main-medium text_color_inactive',
+          )}
+        >
+          История заказов
+        </NavLink>
+        <NavLink
+          className={classNames(
+            styles.link,
+            location.pathname === '/logout'
+              ? ['text text_type_main-medium', styles.linkActive]
+              : 'text text_type_main-medium text_color_inactive',
+          )}
+        >
+          Выход
+        </NavLink>
+        <p className={classNames('text text_type_main-default text_color_inactive', styles.caption)}>
+          В этом разделе вы можете изменить свои персональные данные
+        </p>
+      </nav>
+      <form>
+        <Input
+          type={'text'}
+          placeholder={'Имя'}
+          onChange={(e) => setNameValue(e.target.value)}
+          value={nameValue}
+          name={'name'}
+          error={false}
+          ref={inputRef}
+          minLength={2}
+          errorText={'Ошибка'}
+          size={'default'}
+          icon={'EditIcon'}
+          extraClass='mb-6'
+          required
+        />
+        <Input
+          type={'email'}
+          placeholder={'Логин'}
+          onChange={(e) => setEmailValue(e.target.value)}
+          value={emailValue}
+          name={'email'}
+          error={false}
+          ref={inputRef}
+          pattern={emailRegExp}
+          errorText={'Ошибка'}
+          size={'default'}
+          icon={'EditIcon'}
+          extraClass='mb-6'
+          required
+        />
+        <Input
+          type={'password'}
+          placeholder={'Пароль'}
+          onChange={(e) => setPasswordValue(e.target.value)}
+          value={passwordValue}
+          name={'password'}
+          error={false}
+          ref={inputRef}
+          errorText={'Ошибка'}
+          size={'default'}
+          icon={'EditIcon'}
+          extraClass='mb-6'
+          minLength={8}
+          required
+        />
+      </form>
     </section>
   );
 };
